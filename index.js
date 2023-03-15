98689aacfd9ce0eb494c802f91a6cbdeff12603f
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-15 11:52:32 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader); adds the new element to the body
+
+newHeader.id = "victory";
+
+newHeader.textContent = "William is the champion";
